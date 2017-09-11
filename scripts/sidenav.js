267fbf7fd712416029c11da5fd756a91290e075c
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-11 20:02:00 -0300

    test -

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,18 +1,21 @@
+baseColor= document.body.style['background-color']
+
 function openNav(){
   if($(window).width() > 600){
     document.getElementById("lateral-menu").style.width = "170px";
-    //document.getElementById("right-arrow").style["margin-left"] = "25%";
     document.getElementById("main").style["margin-left"] = "170px";
   }else{
     document.getElementById("lateral-menu").style.width = "75%";
-    //document.getElementById("right-arrow").style["margin-left"] = "75%";
     document.getElementById("main").style["margin-left"] = "75%";
   }
-  console.log(document.getElementById("lateral-menu").clientWidth);
+  //document.body.style['background-color'] = "black";
+  //document.body.style['opacity'] = "0.5";
 }
 
 function closeNav(){
   document.getElementById("lateral-menu").style.width = "0%";
   document.getElementById("right-arrow").style["margin-left"] = "0";
   document.getElementById("main").style["margin-left"] = "0";
+  //document.body.style['background-color'] = baseColor;
+  //document.body.style['opacity'] = "1";
 }
