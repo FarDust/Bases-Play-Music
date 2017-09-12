d154ce96264f0268711d25366cea1aa5db28c592
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-12 00:10:34 -0300

    Lateral menu fix

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,8 +1,7 @@
 baseColor= document.body.style['background-color']
-var open;
+var open = false;
 
 function openNav(){
-  open = true;
   if($(window).width() > 600){
     document.getElementById("lateral-menu").style.width = "170px";
     document.getElementById("main").style["margin-left"] = "170px";
@@ -12,17 +11,19 @@ function openNav(){
     document.getElementById("main").style["margin-left"] = "75%";
     if (open == true){
       closeNav();
+      return;
     }
+    open = true;
   }
   //document.body.style['background-color'] = "black";
   //document.body.style['opacity'] = "0.5";
 }
 
 function closeNav(){
-  open = false;
   document.getElementById("lateral-menu").style.width = "0%";
   document.getElementById("right-arrow").style["margin-left"] = "0";
   document.getElementById("main").style["margin-left"] = "0";
   //document.body.style['background-color'] = baseColor;
   //document.body.style['opacity'] = "1";
+  open = false;
 }
