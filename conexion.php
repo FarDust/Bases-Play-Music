d5af06bd82543b5957a4216e88b6b483d560ddae
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-11 21:20:56 -0300

    more fixes

--- conexion.php
+++ conexion.php
@@ -8,8 +8,8 @@
       include(".credentials.php");
       $query = $_POST["query"];
       $sql = ''.trim($query).'';
-      if ($query = "0"){
-        $sql = 'SELECT * FROM test'
+      if ($query == "0"){
+        $sql = 'SELECT * FROM test';
       }
       $db = pg_connect("host=localhost dbname=".trim($dbname)." user=".trim($dbuser)." password=".trim($dbpass)."") or die('Conection failed: '.pg_last_error());
       $result= pg_query($sql) or die('Query failed: '.pg_last_error());
