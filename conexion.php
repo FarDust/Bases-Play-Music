b6390e078fba1e3a548e223d4c393aa517f6c3d9
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-11 21:19:12 -0300

    Static SQL Fix

--- conexion.php
+++ conexion.php
@@ -5,9 +5,12 @@
   <body>
     <div id="errors" style="display: none;">
       <?php
-	  include(".credentials.php");
+      include(".credentials.php");
       $query = $_POST["query"];
       $sql = ''.trim($query).'';
+      if ($query = "0"){
+        $sql = 'SELECT * FROM test'
+      }
       $db = pg_connect("host=localhost dbname=".trim($dbname)." user=".trim($dbuser)." password=".trim($dbpass)."") or die('Conection failed: '.pg_last_error());
       $result= pg_query($sql) or die('Query failed: '.pg_last_error());
       pg_close();
