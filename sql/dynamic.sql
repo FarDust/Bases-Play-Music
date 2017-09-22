02d3da723322d066fe2966f989146d9375fef154
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-22 14:59:50 -0300

    last commit

--- sql/dynamic.sql
+++ sql/dynamic.sql
@@ -1,1 +1,1 @@
-SELECT * FROM Artista, Banda, Miembro WHERE Artista.id = Miembro.ida AND Banda.id = Miembro.idb AND Artista.nombre = $1
+SELECT Artista.* FROM Artista, Banda, Miembro WHERE Artista.id = Miembro.ida AND Banda.id = Miembro.idb AND Artista.nombre = $1
