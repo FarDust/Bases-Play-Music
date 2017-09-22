855ca7030cca319f431faf73ccbbdc5952b82f19
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-22 12:25:15 -0300

    fixed?

--- conexion.php
+++ conexion.php
@@ -7,22 +7,23 @@
       <?php
       include(".credentials.php");
       include("sqlread.php");
-      if (isset($_POST['query'])){
+      elseif (isset($_POST['query'])){
         $query = $_POST["query"];
         $sql = ''.trim($query).'';
       }
-      if (isset($_POST['static'])){
+      elseif (isset($_POST['static'])){
         echo $staticSQL[$_POST['static']];
         $sql = $staticSQL[$_POST['static']];
       }
-  	  if (isset($_POST['dynamic'])){
-  		  echo $dynamicSQL[$_POST['dynamic']];
-  		  $sql = $dynamicSQL[$_POST['dynamic']];
+  	  elseif (isset($_POST['dynamic'])){
+  		  echo $dynamicSQL[$_POST['option']];
+  		  $sql = $dynamicSQL[$_POST['option']];
   	  }
       $db = pg_connect("host=localhost dbname=".trim($dbname)." user=".trim($dbuser)." password=".trim($dbpass)."") or die('Conection failed: '.pg_last_error());
   	  if (isset($_POST['dynamic'])){
         unset($_POST['dynamic']);
-        var_dump($_POST);;
+        unset($_POST['option']);
+        var_dump($_POST);
   		  $result = pg_query_params($db,$sql,$_POST) or die('Query failed: '.pg_last_error());
   	  }else {
   	    $result= pg_query($sql) or die('Query failed: '.pg_last_error());
