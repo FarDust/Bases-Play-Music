6941479cc5136d070609041620ef6b4bee67caee
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-22 14:15:02 -0300

    bugs

--- sql/dynamic.sql
+++ sql/dynamic.sql
@@ -1,1 +1,1 @@
-SELECT * FROM Artista, Banda, Miembro WHERE Artista.id = aid Banda.id = Miembro.bid AND Artista.nombre = $1;
+SELECT * FROM Artista, Banda, Miembro WHERE Artista.id = aid AND Banda.id = Miembro.bid AND Artista.nombre = $1
