8394c575d17c20b6702e156b129699e83c6d6029
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-23 15:40:44 -0300

    fix

--- scripts/sqlreader.js
+++ scripts/sqlreader.js
@@ -1,5 +1,5 @@
 var doc= document.querySelector('link[id="sql"]').import;
-var table = document.getElementById("tables");
+var tablet = document.getElementById("tables");
 var tables = doc.body.innerHTML.split("SELECT * FROM ").slice(1);
 
 for (var i = 0; i < tables.length; i++){
@@ -7,5 +7,5 @@ for (var i = 0; i < tables.length; i++){
   option = document.createElement("OPTION");
   option.value = i;
   option.innerHTML = current;
-  table.appendChild(option);
+  tablet.appendChild(option);
 }
