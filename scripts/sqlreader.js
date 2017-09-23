11c6eeb3488859a5921d9b9670a2543a34f30ecc
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-23 14:54:32 -0300

    read2

--- scripts/sqlreader.js
+++ scripts/sqlreader.js
@@ -1,3 +1,3 @@
 var doc= document.querySelector('link[id="sql"]').import;
 var tables = document.getElementById("tables");
-console.log(doc.body.toString());
+console.log(doc.getElementByTagName("BODY")[0].toString());
