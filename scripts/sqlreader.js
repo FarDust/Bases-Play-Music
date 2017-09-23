65f1aa7156643c95c4f577e785652fab6b1b3ed8
Gabriel Faundez <gnfaundez@uc.cl>
2017-09-23 15:18:38 -0300

    update

--- scripts/sqlreader.js
+++ scripts/sqlreader.js
@@ -1,3 +1,11 @@
 var doc= document.querySelector('link[id="sql"]').import;
-var tables = document.getElementById("tables");
-console.log(doc.getElementByTagName("BODY")[0].toString());
+var table = document.getElementById("tables");
+var tables = doc.body.innerHTML.split("SELECT * FROM ").slice(1);
+
+for (var i = 0; i < tables.length; i++){
+  current = tables[i].replace(/(\r\n|\n|\r)/gm,"");
+  option = document.createElement("OPTION");
+  option.value = i;
+  option.innerHTML = current;
+  table.appendChild(option);
+}
