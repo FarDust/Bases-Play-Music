1dc9bfd284ec4c923fdae7c4d3557ffd213cc15b
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-16 23:28:15 -0300

    finding people

---
+++ scripts/find.js
@@ -0,0 +1,17 @@
+function fullyload(){
+  var ia = document.getElementById('integrantes-a');
+  var ir = document.getElementById('integrates-r');
+  var disc = document.getElementById('discs');
+  var textArea = iframe.contentDocument.getElementById("result");
+  console.log(textArea);
+  if (textArea){
+    var array = JSON.parse(textArea.innerHTML);
+    console.log(array);
+
+    for (key in array['a_members']){
+      var t = document.createTextNode(array[key]);
+      var node = document.createElement("li");
+      ia.appendChild(t);
+    }
+  }
+}
